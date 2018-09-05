19d3d9f31e354bd6684c14968d3949444b7b7b09
Jose Bejarano Carranza <jabcarranz@gmail.com>
2018-09-04 20:50:13 -0500

    Boton de registro movido/creacion pantalla de eliminar

---
+++ app/src/main/java/com/arsenic/jose/strack/View/DelActivity.java
@@ -0,0 +1,15 @@
+package com.arsenic.jose.strack.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arsenic.jose.strack.R;
+
+public class DelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_del);
+    }
+}
